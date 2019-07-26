fdf95a2f38dbbe8f891f9e6cd1b516082612faee
riyad12 <mangekujang2@gmail.com>
2019-07-27 01:25:46 +0700

    Update erp-utama.js

--- erp-utama.js
+++ erp-utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/12/6-benefits-of-machine-learning-and-ai.html"}
 if(x=="2"){xxx="/2018/12/how-to-manage-change-during-merger-or.html"}
 if(x=="3"){xxx="/2018/12/5-benefits-of-business-process.html"}
 if(x=="4"){xxx="/2018/12/6-ways-to-mitigate-software.html"}
-if(x=="5"){xxx="/2018/12/how-to-improve-your-sales-processes-and.html"}a_to_ve[i].href="https://erpsoftware.airlites.us"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/12/how-to-improve-your-sales-processes-and.html"}a_to_ve[i].href="https://moneywikii.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
